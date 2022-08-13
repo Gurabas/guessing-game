28d88a312d33d1ded963220a3d9c288515781cd7
Gurabas <cuper8555@gmail.com>
2022-08-13 08:56:50 +0300

    complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
-
+    setRange(mini, maxi) {
+        this.mini = mini;
+        this.maxi = maxi;
+        this.midi = Math.ceil((this.mini + this.maxi) / 2);
     }
 
     guess() {
-
+        return this.midi;
     }
 
     lower() {
+        this.maxi = this.midi;
+        this.midi = Math.ceil((this.mini + this.maxi) / 2);
 
     }
 
     greater() {
-
+        this.mini = this.midi;
+        this.midi = Math.ceil((this.mini + this.maxi) / 2);
     }
 }
 
